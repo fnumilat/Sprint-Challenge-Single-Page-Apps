e9ff6c2cc34e15f5781e6ac90e40394924c35347
unknown <fnumilat@gmail.com>
2020-01-25 14:13:33 -0600

    work progressed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const [char, setChar] = useState([]);
+
+  useEffect(() => {
+    const id = props.match.params.id;
+    // change ^^^ that line and grab the id from the URL
+    // You will NEED to add a dependency array to this effect hook
+
+       axios
+        .get(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(response => {
+          setChar(response.data.result);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+  },[]);
+
+  const { image, name } = char;
+  return (
+    <div>
+    <img src={image}/>
+    <h2>Character's Name: {name}</h2>
+  </div>
+
+  );
 }
+
+
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,44 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function CharacterList() {
+function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(res => setData(res.data.results))
+    .catch(err => console.log(err))
   }, []);
+  console.log(data)
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
+      <div>
+      {data.map((char) => {
+          return <CharacterDetails key={char.id} char={char} />
+        })}
+      </div>
     </section>
   );
 }
+
+function CharacterDetails({char}) {
+  const { image, name } = char;
+  return (
+    <div>
+      <Link to={`/CharacterCard/${char.id}`}>
+      <img src={image}/>
+      <h2>Character's Name: {name}</h2>
+      </Link>
+    </div>
+
+  )
+}
+
+export default CharacterList;
